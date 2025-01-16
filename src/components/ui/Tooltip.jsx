d87ebfc09957976fec5cd40f3c7df39a6975467c
Stephen <stephen.t.j.leong@gmail.com>
2025-01-16 00:59:55 +0000

    fix tooltip animation

--- src/components/ui/Tooltip.jsx
+++ src/components/ui/Tooltip.jsx
@@ -11,9 +11,9 @@ const Tooltip = () => {
         // Hide tooltip after 1 second
         setTimeout(() => {
           setShowTooltip(false);
-        }, 2000); // Tooltip disappears after 1 second
+        }, 4000); // Tooltip disappears after 1 second
   
-      }, Math.floor(Math.random() * (5000 - 4000 + 1)) + 4000); // Random interval between 3 and 5 seconds
+      }, Math.floor(Math.random() * (9000 - 8000 + 1)) + 8000); // Random interval between 3 and 5 seconds
   
       // Clean up interval when component is unmounted
       return () => clearInterval(interval);
@@ -23,7 +23,8 @@ const Tooltip = () => {
         <>
         {/* Tooltip */}
         {showTooltip && (
-            <div className="absolute animate-pulse-in transform -translate-x-1/2 px-4 py-2 bg-gray-700 text-white text-sm rounded-md shadow-lg">
+            <div className="absolute animate-pulse-in transform -translate-x-1/2 px-4 py-2 bg-gray-700 text-white text-sm rounded-md shadow-lg"
+            style={{ transform: "translateZ(0)", willChange: "opacity, transform" }}>
                 There's something I need to ask you 
             </div>
         )}
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,7 @@ export default {
   theme: {
     extend: {
       animation: {
-        'pulse-in': 'pulseIn 2s ease-in-out',
+        'pulse-in': 'pulseIn 4s ease-in-out',
       },
       keyframes: {
         pulseIn: {
@@ -15,33 +15,9 @@ export default {
             transform: 'scale(1)',
             opacity: '0',
           },
-          '12.5%': {
-            transform: 'scale(1.05)',
-            opacity: '0.25',
-          },
-          '25%': {
-            transform: 'scale(1.1)',
-            opacity: '0.5',
-          },
-          '37.5%': {
-            transform: 'scale(1.15)',
-            opacity: '0.75',
-          },
           '50%': {
             transform: 'scale(1.2)',
             opacity: '1',
-          },
-          '52.5%': {
-            transform: 'scale(1.25)',
-            opacity: '0.75',
-          },
-          '75%': {
-            transform: 'scale(1.3)',
-            opacity: '0.5',
-          },
-          '87.5%': {
-            transform: 'scale(1.35)',
-            opacity: '0.25',
           },
           '100%': {
             transform: 'scale(1.4)',
